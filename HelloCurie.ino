9eda0ad773f5dfacc9d29be54e726a752bb5454e
ishotjr <github@ishotjr.com>
2017-03-12 01:32:40 -0500

    :octocat: :tada: Initial commit

---
+++ HelloCurie.ino
@@ -0,0 +1,39 @@
+/*
+ * -----------------------------------------------------------------------------
+ *
+ * HelloCurie - getting to know the Arduino 101!
+ * 
+ * -----------------------------------------------------------------------------
+ *
+ */
+
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+const int color_r = 255;
+const int color_g = 0;
+const int color_b = 0;
+
+
+void setup() {
+  
+  // set up LCD rows/cols
+  lcd.begin(16,2);
+
+  // set LCD backlight color
+  lcd.setRGB(color_r, color_g, color_b);
+  
+  lcd.print("Hello, Curie!");
+}
+
+void loop() {
+
+  // set cursor to beginning of second (numbered from 0) row
+  lcd.setCursor(0, 1);
+
+  lcd.print(millis() / 1000);
+
+  delay(100);
+}
