b5f0cdf8ff04aff37425792da07391b8e4224c28
ishotjr <github@ishotjr.com>
2017-03-19 03:40:07 -0400

    :signal_strength: :art: Add remote backlight color setting via BLE

--- HelloCurie.ino
+++ HelloCurie.ino
@@ -7,6 +7,9 @@
  *
  */
 
+// for Curie Bluetooth Low Energy (BLE)
+#include <CurieBLE.h>
+
 // for Curie inertial measurement unit (BMI160)
 #include "CurieIMU.h"
 
@@ -26,6 +29,12 @@ const int piezo_pin = 4;
 const int led_pin = 5;      // must support PWM
 
 
+BLEPeripheral blePeripheral;
+BLEService lcdService("19B10010-E8F2-537E-4F6C-D104768A1214");
+BLEUnsignedCharCharacteristic redCharacteristic("19B10011-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+BLEUnsignedCharCharacteristic greenCharacteristic("19B10012-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+BLEUnsignedCharCharacteristic blueCharacteristic("19B10013-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+
 rgb_lcd lcd;
 
 static byte color_r = 255;
@@ -37,6 +46,24 @@ void setup() {
 
   // randomize based on unconnected pin noise
   randomSeed(analogRead(0));
+
+
+  // initialize Curie BLE, services and characteristics
+  blePeripheral.setLocalName("HelloCurie");
+  // set advertised service UUID
+  blePeripheral.setAdvertisedServiceUuid(lcdService.uuid());
+
+  blePeripheral.addAttribute(lcdService);
+  blePeripheral.addAttribute(redCharacteristic);
+  blePeripheral.addAttribute(greenCharacteristic);
+  blePeripheral.addAttribute(blueCharacteristic);
+
+  redCharacteristic.setValue(color_r);
+  greenCharacteristic.setValue(color_r);
+  blueCharacteristic.setValue(color_r);
+
+  blePeripheral.begin();
+
 
   // initialize Curie IMU
   CurieIMU.begin();
@@ -68,7 +95,9 @@ void setup() {
 }
 
 void loop() {
-
+  // poll for BLE events
+  blePeripheral.poll();
+  
   // set cursor to beginning of second (numbered from 0) row
   lcd.setCursor(0, 1);
 
@@ -79,6 +108,23 @@ void loop() {
     analogWrite(led_pin, 0);
   } else {
     analogWrite(led_pin, 255);
+  }
+
+  // TODO: make BLE/button exclusive/prioritize one or the other?
+  if (redCharacteristic.written()) {
+    // TODO: error checking?
+    color_r = redCharacteristic.value();
+    UpdateBacklightColor();
+  }
+  if (greenCharacteristic.written()) {
+    // TODO: error checking?
+    color_g = greenCharacteristic.value();
+    UpdateBacklightColor();
+  }
+  if (blueCharacteristic.written()) {
+    // TODO: error checking?
+    color_b = blueCharacteristic.value();
+    UpdateBacklightColor();
   }
 
   //lcd.print(millis() / 1000);
@@ -98,13 +144,18 @@ void loop() {
   delay(100);
 }
 
+void UpdateBacklightColor() {
+  
+  // set LCD backlight color
+  lcd.setRGB(color_r, color_g, color_b);
+}
+
 void SetRandomBacklightColor() {
 
   color_r = random(256);
   color_g = random(256);
   color_b = random(256);
   
-  // set LCD backlight color
-  lcd.setRGB(color_r, color_g, color_b);
+  UpdateBacklightColor();
 }
 
