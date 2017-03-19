9916a4ee191c6ae8bd83cd6cf180d69b43a6000d
ishotjr <github@ishotjr.com>
2017-03-19 00:14:46 -0400

    :confounded: :hotsprings: Additional failed thermistor calculations

--- HelloCurie.ino
+++ HelloCurie.ino
@@ -79,6 +79,7 @@ void loop() {
     resistance = (float)(1023 - sensor_value_tmp) * 10000 / sensor_value_tmp
     temperature = round((float)(1 / (math.log(resistance / 10000) / bValue + 1 / 298.15) - 273.15), 2)
  */
+ /*
  // TODO: move to own fn etc.
   float thermistor_value = analogRead(thermistor_pin);
   int b_value = 4250; //4275; //4200; //4250;
@@ -97,7 +98,59 @@ void loop() {
   lcd.setCursor(8, 1);
   float temperature_f = temperature_c * 9/5 + 32;
   lcd.print(temperature_f);
+*/
+/*
+  float thermistor_value = analogRead(thermistor_pin);
+  float average;
+  average = 1023 / thermistor_value - 1;
+  average = 100000 / average;
+  //average = 100000 * average;
+  lcd.print(average);
 
+  float steinhart;
+  steinhart = average / 100000;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= 4275;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (25 + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+
+  lcd.setCursor(11, 1);
+  //lcd.print(steinhart);
+  float temperature_f = steinhart * 9/5 + 32;
+  lcd.print(temperature_f);
+*/
+/*
+  //float thermistor_value = analogRead(thermistor_pin);
+  int thermistor_value = analogRead(thermistor_pin);
+  float R = 1023.0/((float)thermistor_value)-1.0;  
+  //R = 100000.0*R;
+  R = 100000.0/R;
+ 
+  float temperature=1.0/(log(R/100000.0)/4275+1/298.15)-273.15;//convert to temperature via datasheet ;
+
+  float steinhart;
+  steinhart = R / 100000;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= 4275;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (25 + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+  
+  lcd.print(temperature);
+  lcd.setCursor(8, 1);
+  //float temperature_f = steinhart * 9/5 + 32;
+  //lcd.print(temperature_f);
+  lcd.print(steinhart);
+
+  delay(100);
+*/
+//resistance = (float)(1023 - a) * 10000 / a
+//temperature = (float) (1 / (math.log(resistance / 10000) / bValue + 1 / 298.15) - 273.15)
+  int thermistor_value = analogRead(thermistor_pin);
+  float R = (float)(1023 - thermistor_value) * 100000 / thermistor_value;
+  float temperature = (float) (1 / (log(R / 100000) / 4275 + 1 / 298.15) - 273.15);
+  lcd.print(temperature);
   delay(100);
 }
 
