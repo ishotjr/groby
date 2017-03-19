2c67b28d45c6599c79beb0c035ca8df68f8d0f76
ishotjr <github@ishotjr.com>
2017-03-19 02:13:49 -0400

    :left_right_arrow: :arrow_up_down: Add accelerometer

--- HelloCurie.ino
+++ HelloCurie.ino
@@ -7,6 +7,9 @@
  *
  */
 
+// for Curie inertial measurement unit (BMI160)
+#include "CurieIMU.h"
+
 // for Grove LCD RGB Backlight
 #include <Wire.h>
 #include "rgb_lcd.h"
@@ -34,6 +37,13 @@ void setup() {
 
   // randomize based on unconnected pin noise
   randomSeed(analogRead(0));
+
+  // initialize Curie IMU
+  CurieIMU.begin();
+
+  // set accelerometer range to 2G
+  CurieIMU.setAccelerometerRange(2);
+  
 
   // set up button
   pinMode(button_pin, INPUT);
@@ -72,85 +82,19 @@ void loop() {
   }
 
   //lcd.print(millis() / 1000);
-
-  // TODO: fix calculation; 
-/*
-    sensor_value_tmp = (float)(average_value / 4095 * 2.95 * 2 / 3.3 * 1023)
-    resistance = (float)(1023 - sensor_value_tmp) * 10000 / sensor_value_tmp
-    temperature = round((float)(1 / (math.log(resistance / 10000) / bValue + 1 / 298.15) - 273.15), 2)
- */
- /*
- // TODO: move to own fn etc.
-  float thermistor_value = analogRead(thermistor_pin);
-  int b_value = 4250; //4275; //4200; //4250;
-  // TODO: 3.3 or 5.0?
-  float x = (float)(thermistor_value / 4095 * 2.95 * 2 / 3.3 * 1023);
-  //float x = (float)(thermistor_value / 4095 * 2.95 * 2 / 5.0 * 1023);
   
-  float resistance = (float)(1023 - x) * 10000 / x;
-  float temperature_c = (float)(1 / (log(resistance / 10000) / b_value + 1 / 298.15) - 273.15);
-  //float resistance = (float)(1023 - x) * 100000 / x;
-  //float temperature_c = (float)(1 / (log(resistance / 100000) / b_value + 1 / 298.15) - 273.15);
+  float ax, ay, az;   //scaled accelerometer values
 
-  //lcd.print(temperature_c);
-  lcd.print(thermistor_value);
+  // read accelerometer measurements from device, scaled to the configured range
+  CurieIMU.readAccelerometerScaled(ax, ay, az);
+  lcd.print(ax);
 
-  lcd.setCursor(8, 1);
-  float temperature_f = temperature_c * 9/5 + 32;
-  lcd.print(temperature_f);
-*/
-/*
-  float thermistor_value = analogRead(thermistor_pin);
-  float average;
-  average = 1023 / thermistor_value - 1;
-  average = 100000 / average;
-  //average = 100000 * average;
-  lcd.print(average);
+  lcd.setCursor(6, 1);
+  lcd.print(ay);
 
-  float steinhart;
-  steinhart = average / 100000;     // (R/Ro)
-  steinhart = log(steinhart);                  // ln(R/Ro)
-  steinhart /= 4275;                   // 1/B * ln(R/Ro)
-  steinhart += 1.0 / (25 + 273.15); // + (1/To)
-  steinhart = 1.0 / steinhart;                 // Invert
-  steinhart -= 273.15;                         // convert to C
+  lcd.setCursor(12, 1);
+  lcd.print(az);
 
-  lcd.setCursor(11, 1);
-  //lcd.print(steinhart);
-  float temperature_f = steinhart * 9/5 + 32;
-  lcd.print(temperature_f);
-*/
-/*
-  //float thermistor_value = analogRead(thermistor_pin);
-  int thermistor_value = analogRead(thermistor_pin);
-  float R = 1023.0/((float)thermistor_value)-1.0;  
-  //R = 100000.0*R;
-  R = 100000.0/R;
- 
-  float temperature=1.0/(log(R/100000.0)/4275+1/298.15)-273.15;//convert to temperature via datasheet ;
-
-  float steinhart;
-  steinhart = R / 100000;     // (R/Ro)
-  steinhart = log(steinhart);                  // ln(R/Ro)
-  steinhart /= 4275;                   // 1/B * ln(R/Ro)
-  steinhart += 1.0 / (25 + 273.15); // + (1/To)
-  steinhart = 1.0 / steinhart;                 // Invert
-  steinhart -= 273.15;                         // convert to C
-  
-  lcd.print(temperature);
-  lcd.setCursor(8, 1);
-  //float temperature_f = steinhart * 9/5 + 32;
-  //lcd.print(temperature_f);
-  lcd.print(steinhart);
-
-  delay(100);
-*/
-//resistance = (float)(1023 - a) * 10000 / a
-//temperature = (float) (1 / (math.log(resistance / 10000) / bValue + 1 / 298.15) - 273.15)
-  int thermistor_value = analogRead(thermistor_pin);
-  float R = (float)(1023 - thermistor_value) * 100000 / thermistor_value;
-  float temperature = (float) (1 / (log(R / 100000) / 4275 + 1 / 298.15) - 273.15);
-  lcd.print(temperature);
   delay(100);
 }
 
